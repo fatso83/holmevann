b3f82d9176d1880c168a939a7441857d7beb9dfa
Carl-Erik Kopseng <carlerik@gmail.com>
2019-07-04 12:12:15 +0200

    That seems about right

--- _scripts/build.sh
+++ _scripts/build.sh
@@ -3,5 +3,4 @@
 find docs -type f | xargs git update-index --no-assume-unchanged 2>/dev/null
 bundle exec jekyll build
 find docs -type f | git add -f
-find docs -type f | xargs git update-index --no-assume-unchanged 2>/dev/null
 
