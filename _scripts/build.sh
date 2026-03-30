20a65b33e4bc94e947a9d86d8caf9be71b28e1ae
Carl-Erik Kopseng <carlerik@gmail.com>
2026-03-30 15:44:02 +0200

    remove old scripts not in use

--- _scripts/build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-find docs -type f -exec git update-index --no-assume-unchanged {} \; 2>/dev/null
-bundle exec jekyll build
-find docs -type f | xargs git add -f
-
--- _scripts/serve.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-find docs -type f -exec git update-index --assume-unchanged {} \; 2>/dev/null
-bundle exec jekyll serve
-
-
